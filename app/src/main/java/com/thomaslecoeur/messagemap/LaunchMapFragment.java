ebf249bb9998e33b7ba0baee6418bb0d9bd53c9f
Thomas Lecoeur <me@thomaslecoeur.com>
2016-01-31 15:33:04 +0100

    Setup map view and basic navigation between views

---
+++ app/src/main/java/com/thomaslecoeur/messagemap/LaunchMapFragment.java
@@ -0,0 +1,84 @@
+package com.thomaslecoeur.messagemap;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LaunchMapFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class LaunchMapFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public LaunchMapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_launch_map, container, false);
+
+        Button myButton = (Button) view.findViewById(R.id.openMapButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOpenMapButtonPressed();
+            }
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onOpenMapButtonPressed() {
+        if (mListener != null) {
+            mListener.onFragmentInteraction();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction();
+    }
+}
--- app/src/main/java/com/thomaslecoeur/messagemap/MainActivity.java
+++ app/src/main/java/com/thomaslecoeur/messagemap/MainActivity.java
@@ -1,13 +1,30 @@
 package com.thomaslecoeur.messagemap;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LaunchMapFragment.OnFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.openMapButtonWrapper, new LaunchMapFragment())
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        Log.d(TAG, "click");
+        startActivity(new Intent(MainActivity.this, MapActivity.class));
     }
 }
