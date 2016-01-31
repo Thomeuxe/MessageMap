a816e0586cf83650d71ef5d6acef8f71ac67941d
Thomas Lecoeur <me@thomaslecoeur.com>
2016-01-31 16:20:38 +0100

    Setup MapBox map

--- app/src/main/java/com/thomaslecoeur/messagemap/MapActivityFragment.java
+++ app/src/main/java/com/thomaslecoeur/messagemap/MapActivityFragment.java
@@ -6,10 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MapActivityFragment extends Fragment {
+
+    private static final String TAG = "MAP_FRAGMENT";
+    MapView mapView;
 
     public MapActivityFragment() {
     }
@@ -17,6 +25,59 @@ public class MapActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.setAccessToken(BuildConfig.MAPBOX_ACCESS_TOKEN);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setLatLng(new LatLng(40.73581, -73.99155));
+        mapView.setZoom(11);
+
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(40.73581, -73.99155))
+                .title("Hello World!")
+                .snippet("Welcome to my marker."));
+
+        mapView.onCreate(savedInstanceState);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
